eb7e0d0c61124b27d2d78c42cf7448d3edb4c93a
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-10-11 21:15:01 +0100

    feat: define get certificate methods

--- src/godaddy_reseller_client/certificates.py
+++ src/godaddy_reseller_client/certificates.py
@@ -6,6 +6,38 @@ class certificate:
         self.headers = {
             'accept': 'application/json',
             'Content-Type': 'application/json',
-            'Auhthorization': 'sso-key {}:{}'.format(key,secret)
+            'Auhthorization': 'sso-key {}:{}'.format(key, secret)
         }
-    
+
+    def get_certificate(self):
+        pass
+
+    def get_certificate_actions(self):
+        pass
+
+    def get_certificate_callback(self):
+        pass
+
+    def get_customer_certificate_details(self):
+        pass
+
+    def get_customer_certificates(self):
+        pass
+
+    def get_detailed_info_for_supplied_domain(self):
+        pass
+
+    def get_domain_verification_status(self):
+        pass
+
+    def get_download(self):
+        pass
+
+    def get_email_history(self):
+        pass
+
+    def get_external_account_binding_for_customer(self):
+        pass
+
+    def get_site_seal(self):
+        pass
