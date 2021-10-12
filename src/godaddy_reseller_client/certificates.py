07e53cf6eb571aaa859e692ba12bee0105789b4f
Omar-Belghaouti <bel_omar18@yahoo.com>
2021-10-12 15:42:46 +0100

    feat: implementing certificates get methods

--- src/godaddy_reseller_client/certificates.py
+++ src/godaddy_reseller_client/certificates.py
@@ -1,3 +1,6 @@
+import requests
+
+
 class certificate:
     def __init__(self, base_url, key, secret):
         self.base_url = base_url
@@ -9,38 +12,103 @@ class certificate:
             'Auhthorization': 'sso-key {}:{}'.format(key, secret)
         }
 
-    def get_certificate(self):
-        pass
+    def get_certificate(self, certificate_id):
+        req_url = '{}/v1/certificates/{}'.format(self.base_url, certificate_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_certificate_actions(self):
-        pass
+    def get_certificate_actions(self, certificate_id):
+        req_url = '{}/v1/certificates/{}/actions'.format(
+            self.base_url, certificate_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_certificate_callback(self):
-        pass
+    def get_certificate_callback(self, certificate_id):
+        req_url = '{}/v1/certificates/{}/callback'.format(
+            self.base_url, certificate_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_customer_certificate_details(self):
-        pass
+    def get_customer_certificate_details(self, customer_id, certificate_id):
+        req_url = '{}/v2/customers/{}/certificates/{}'.format(
+            self.base_url, customer_id, certificate_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_customer_certificates(self):
-        pass
+    def get_customer_certificates(self, customer_id, offset=0, limit=100):
+        req_url = '{}/v2/customers/{}/certificates?offset={}&limit={}'.format(
+            self.base_url, customer_id, offset, limit)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_detailed_info_for_supplied_domain(self):
-        pass
+    def get_detailed_info_for_supplied_domain(self, customer_id, certificate_id, domain):
+        req_url = '{}/v2/customers/{}/certificates/{}/domainVerifications/{}'.format(
+            self.base_url, customer_id, certificate_id, domain)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_domain_verification_status(self):
-        pass
+    def get_domain_verification_status(self, customer_id, certificate_id):
+        req_url = '{}/v2/customers/{}/certificates/{}/domainVerifications'.format(
+            self.base_url, customer_id, certificate_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_download(self):
-        pass
+    def get_download(self, certificate_id):
+        req_url = '{}/v1/certificates/{}/download'.format(
+            self.base_url, certificate_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_email_history(self):
-        pass
+    def get_email_history(self, certificate_id):
+        req_url = '{}/v1/certificates/{}/email/history'.format(
+            self.base_url, certificate_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_external_account_binding_for_customer(self):
-        pass
+    def get_external_account_binding_for_customer(self, customer_id):
+        req_url = '{}/v2/customers/{}/cartificates/acme/externalAccountBindings'.format(
+            self.base_url, customer_id)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
-    def get_site_seal(self):
-        pass
+    def get_site_seal(self, certificate_id, theme, local):
+        req_url = '{}/v1/certificates/{}/siteSeal?theme={}&local={}'.format(
+            self.base_url, certificate_id, theme, local)
+        response = requests.get(
+            url=req_url,
+            headers=self.headers
+        )
+        return response.json()
 
     def post_certificate(self):
         pass
